b29afa8653d1c5836bc387f11bf12129bdf577e6
segfaulty1 <segfaulty@proton.me>
2023-02-03 14:11:52 +0100

    feat: implemented mergesort algorithm.

---
+++ src/algos/sorting/merge.ts
@@ -0,0 +1,41 @@
+const merge = (list: any[]) => {
+  const output = [...list];
+
+  const mergeSrt = (outputArg: number[]) => {
+    if (outputArg && outputArg.length < 2) {
+      return outputArg;
+    }
+
+    // split
+    let leftArr = outputArg.slice(0, (outputArg.length / 2));
+    let rightArr = outputArg.slice((outputArg.length / 2));
+    leftArr = mergeSrt(leftArr as []) as [];
+    rightArr = mergeSrt(rightArr as []) as [];
+
+    // sort
+    let sorted = [];
+    let l = 0;
+    let r = 0;
+    while (l < leftArr.length && r < rightArr.length) {
+      if (leftArr[l] < rightArr[r]) {
+        sorted.push(leftArr[l]);
+        l++;
+        continue;
+      }
+
+      sorted.push(rightArr[r]);
+      r++;
+    }
+    if(l < leftArr.length){
+      sorted = [...sorted, ...leftArr.slice(l)]
+    }else{
+      sorted = [...sorted, ...rightArr.slice(r)]
+    }
+
+    return sorted;
+  };
+
+  return mergeSrt(output);
+};
+
+export default merge;
