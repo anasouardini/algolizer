620ad17e794318cc989c4eb24baf348fbc30938c
segfaulty1 <segfaulty@proton.me>
2023-02-04 18:20:46 +0100

    minor: saving before mergin remote into local

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -1,11 +1,15 @@
 import selection from './sorting/selection';
 import insertion from './sorting/insertion';
+import bubble from './sorting/bubble';
+import quick from './sorting/quick';
+import merge from './sorting/merge';
 
+// TODO: visualize memory consumption
 
 // TESTING
 const algoTester = ()=>{
   const input = [3, 55, 5, 0, 12, 2, 4, 23, 1];
-  const output = insertion(input);
+  const output = merge(input);
   console.log('input: ', input);
   console.log('output: ', output);
 }
