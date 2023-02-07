b87288073c93ca60335b4d7bd85842c45372fba0
segfaulty1 <segfaulty@proton.me>
2023-02-07 09:50:07 +0100

    feat: added data generator module.

---
+++ src/genData.ts
@@ -0,0 +1,10 @@
+const sorted =()=>{} 
+const halfSorted =()=>{} 
+const nearlySorted =()=>{} 
+const random =()=>{} 
+const fewUnique =()=>{} 
+const uniformed =()=>{} 
+const uniformedWide =()=>{} 
+
+
+export default [sorted, halfSorted, nearlySorted, random, fewUnique, uniformed, uniformedWide];
