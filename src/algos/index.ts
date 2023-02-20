25fd8c8b2893f832c5f25dd88fc8dc82544aa449
segfaulty1 <segfaulty@proton.me>
2023-02-20 14:48:18 +0100

    minor: tested the loggin ability integration.

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -9,20 +9,21 @@ import quickSelect from './searching/quickSelect';
 import interpolation from './searching/interpolation';
 
 // TODO: visualize memory consumption
+// TODO: use destructuring to swap items
 
 // TESTING
-const algoTester = () => {
-  const input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// const algoTester = () => {
+  // const input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   // const input = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
   // const input = [0, 2, 5, 12, 22, 40, 55, 56];
-  // const input = [3, 55, 5, 0, 12, 2, 4, 23, 1];
-  // const output = interpolation(input, 8);
-  console.log('input: ', input);
-  console.log('output: ', output);
-};
+  const input = [3, 55, 5, 0, 12, 2, 4, 23, 1];
+  // const output = merge(input);
+  // console.log('input: ', input);
+  // console.log('output: ', output);
+// };
 
 // export default algoTester;
 export default {
   sorting: [ selection, insertion, bubble, quick, merge ],
-  searching: [ linear ],
+  searching: [ linear, binaryOrdered, interpolation, quickSelect],
 };
