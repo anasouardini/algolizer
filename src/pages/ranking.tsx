d9ceab9524574a5656feeb28c20712db3c3cf924
segfaulty1 <segfaulty@proton.me>
2023-02-20 14:46:50 +0100

    feat: finished implementing the ranking page. resolves #5.

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import algorithms from '../algos/index';
 import genData from '../genData';
+import { stepsLogT } from '../algos/types';
 
 import AlgoAnimation from '../components/algoAnimation';
 
@@ -9,24 +10,55 @@ export default function Ranking() {
     'sorting'
   );
 
-  const genCells = () => {
-    return genData.map((dataItem) => {
-      return algorithms[tabState].map((algo) => {
-        if (tabState == 'sorting') {
-          return (
-            <td>
-              <AlgoAnimation procedure={[]} />
-            </td>
-          );
-        }
-
-        // TODO: gen a random value to search for
-        // TODO: get searching vlaue from the user
-        // return <td>{algo(dataItem(), )}</td>;
-      });
+  const cbQueueRef = React.useRef<{step: () => void, length: number}[]>([]).current;
+  const runSteps = () => {
+    cbQueueRef.forEach((cb) =>{
+      for(let i=0;i<cb.length;i++){
+        cb.step()
+      }
     });
   };
 
+  const genCells = () => {
+    const mapDataStructureToAlgorithm = (datum: () => number[]) => {
+      const data = datum();
+
+      if (tabState == 'sorting') {
+        return algorithms[tabState].map((algo) => {
+          const stepsLog:stepsLogT = [];
+          console.log(algo.name)
+          algo(data, stepsLog);
+          // console.log('algoRun: ', `[${tabState}]`, datum.name, '=>', algo.name)
+          return (
+            <td key={`${algo.name}-${datum.name}`}>
+              <AlgoAnimation data={data} stepsLog={stepsLog} queue={cbQueueRef}/>
+            </td>
+          );
+        });
+      }
+
+      // TODO: set Random target
+      // TODO: [opt] get searching vlaue from the user
+      return algorithms[tabState].map((algo) => {
+        const stepsLog:stepsLogT = [];
+        algo(data, randomTarget, stepsLog);
+        return (
+          <td key={`${algo.name}-${datum.name}`}>
+            <AlgoAnimation
+              data={datum()}
+              stepsLog={stepsLog}
+              queue={cbQueueRef}
+            />
+          </td>
+        );
+      });
+    };
+
+    return genData.reduce((acc: { [key: string]: JSX.Element[] }, dataItem) => {
+      acc[dataItem.name] = mapDataStructureToAlgorithm(dataItem);
+      return acc;
+    }, {});
+  };
   const tableCells = genCells();
 
   return (
@@ -34,18 +66,25 @@ export default function Ranking() {
       <table>
         <thead>
           <tr>
-            <th>data/algo</th>
+            <th>
+              <button
+                onClick={runSteps}
+                className={`border-blue-400 border-2 rounded-md px-3 py-1`}
+              >
+                play All
+              </button>
+            </th>
             {algorithms[tabState].map((algo) => {
-              return <th>{algo.name}</th>;
+              return <th key={`${algo.name}`}>{algo.name}</th>;
             })}
           </tr>
         </thead>
         <tbody>
-          {genData.map((dataItem, index) => {
+          {Object.keys(tableCells).map((ItemKey) => {
             return (
-              <tr>
-                <th>{dataItem.name}</th>
-                {tableCells[index]}
+              <tr key={`${ItemKey}`}>
+                <th>{ItemKey}</th>
+                {tableCells[ItemKey]}
               </tr>
             );
           })}
