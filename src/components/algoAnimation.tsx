f0586614728cd31dc9b04e9a5d8c6ac375250e19
segfaulty1 <segfaulty@proton.me>
2023-02-20 14:45:56 +0100

    fix: fixed the animated component.

--- src/components/algoAnimation.tsx
+++ src/components/algoAnimation.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { stepsLogT } from '../algos/types';
 
-export default function AlgoAnimation(props: { procedure: any[] }) {
-  return <p className={`p-10 text-center`}>cell</p>;
+type propsT = {
+  // type: 'searching' | 'sorting';
+  stepsLog: stepsLogT;
+  data: number[];
+  queue: (() => void)[];
+};
+
+export default function AlgoAnimation(props: propsT) {
+
+  const [count, setCount] = React.useState(0);
+  const stateActions = {inc: ()=>setCount((st)=>st+1)}
+
+  React.useEffect(() => {
+    const step = () => {stateActions.inc()};
+    props.queue.push({step, length: props.stepsLog.length});
+  }, []);
+
+  const visualizeSteps = () => {
+    // TODO: perform step on visualized data
+  };
+
+  const visualizeData = () => {
+    //
+  };
+  return <p className={`p-10 text-center`}>cell - {count}</p>;
 }
