2c6de899d08eeb288351d51b22d0858072b38329
segfaulty1 <segfaulty@proton.me>
2023-02-22 18:06:20 +0100

    DX: abstracted some functionality.

--- src/components/algoAnimation.tsx
+++ src/components/algoAnimation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { stepsLogT } from '../algos/types';
+import { stepsLogT, logStepTypeT } from '../algos/types';
 
 type propsT = {
   // type: 'searching' | 'sorting';
@@ -12,12 +12,6 @@ type propsT = {
 export default function AlgoAnimation(props: propsT) {
   const barsRefs = React.useRef<(HTMLElement | null)[]>([]).current;
 
-  // TODO: a ref tath points to the bar/li element
-  // TODO: use aboslute positioning for elements animation
-
-  // const [count, setCount] = React.useState(0);
-  // const stateActions = {inc: ()=>setCount((st)=>st+1)}
-
   // LOGGING STEPS
   // if (props.info.algoName == 'selection') {
   //   props.stepsLog.forEach((step) => {
@@ -25,25 +19,37 @@ export default function AlgoAnimation(props: propsT) {
   //   });
   // }
 
-  React.useEffect(() => {
-    const step = () => {
-      props.queue.currentStep++;
+  // TODO: colors are not enough
+  const stepsActionsColor = {
+    idle: ['rgb(147 197 253)'],
+    compare: ['yellow', 'yellow'],
+    swap: ['#bada55', '#bada55'],
+    replace: ['red', 'green'],
+    reduce: ['#ccc'],
+    shift: ['#bada55', '#bada55'],
+    calc: ['#bada55', '#bada55'],
+    push: ['green'],
+    split: ['#bada55', '#bada55'],
+    concat: ['#bada55', '#bada55'],
+  };
 
-      // console.log(barsRefs)
-      // TODO: find a way to fix the types problem
-      // TODO: the second item in elements might be value|index, add a condition
-      // TODO: add a final step in the algos implementation
-      props.stepsLog.map((step) => {
-        if (step.type == 'compare') {
+  React.useEffect(() => {
+    type stepsActionsT = {
+      [key: string]: { do: () => void; undo: () => void };
+    };
+    const stepsActions: stepsActionsT = {
+      compare: { do: () => {
           const firstIndex = step.elements[0].value;
           // const secondIndex = step.elements[1].value;
           // console.log(firstIndex);
           // console.log(props.info.algoName, props.info.dataName);
           // console.log(barsRefs[firstIndex])
 
-          barsRefs[firstIndex].style.background = '#bada55';
-          // barsRefs[secondIndex].style.background = '#bada55';
-        } else if (step.type == 'swap') {
+          barsRefs[firstIndex].style.background = stepsActionsColor[step.type][0];
+          // barsRefs[secondIndex].style.background = stepsActionsColor[step.type][1];
+      }, undo: () => {} },
+      swap: {
+        do: ()=>{
           let tmp;
           const firstIndex = step.elements[0].value;
           const secondIndex = step.elements[1].value;
@@ -52,7 +58,11 @@ export default function AlgoAnimation(props: propsT) {
           tmp = barsRefs[firstIndex];
           barsRefs[firstIndex] = barsRefs[secondIndex];
           barsRefs[secondIndex] = tmp;
-        } else if (step.type == 'replace') {
+        },
+        undo: ()=>{}
+      },
+      replace: {
+        do: ()=>{
           const indexToReplace = step.elements[0].value;
           const valueToReplaceWith = step.elements[1].value;
 
@@ -61,45 +71,86 @@ export default function AlgoAnimation(props: propsT) {
             'data-value',
             barsRefs[valueToReplaceWith]
           );
-        } else if (step.type == 'reduce') {
+
+        },
+        undo: ()=>{}
+      },
+      reduce: {
+        do: ()=>{
           const start = step.range.start;
           const end = step.range.end;
           for (let i = start; i <= end; i++) {
-            barsRefs[i].style.background = '#ccc';
+            barsRefs[i].style.background = stepsActionsColor[step.type][0];
           }
-        } else if (step.type == 'shift') {
+        },
+        undo: ()=>{}
+      },
+      shift: {
+        do: ()=>{
           // TODO: the "to" index is not needed, direction is enough
           const from = step.elements.from;
           const to = step.elements.to;
 
           // shifting an item and shade(kind of hiding) it's first position
           barsRefs[to] = barsRefs[from];
-          barsRefs[from].style.background = '#ccc';
-        } else if (step.type == 'calc') {
+          barsRefs[from].style.background = stepsActionsColor[step.type][0];
+        },
+        undo: ()=>{}
+      },
+      calc: {
+        do: ()=>{
           // TODO: show some calculating animation
-        } else if (step.type == 'push') {
+        },
+        undo: ()=>{}
+      },
+      push: {
+        do: ()=>{
           // TODO: find a way to visualize both of lists and push to the second list
-        } else if (step.type == 'split') {
+        },
+        undo: ()=>{}
+      },
+      split: {
+        do: ()=>{
           for (let i = 0; i < 2; i++) {
             const start = step.chunks[i].start;
             const end = step.chunks[i].end;
 
             // coloring both splited chunks
             for (let k = start; k <= end; k++) {
-              barsRefs[k].style.background = i == 0 ? '#999' : '#ccc';
+              barsRefs[k].style.background = stepsActionsColor[step.type][i];
             }
           }
-        } else if (step.type == 'concat') {
+        },
+        undo: ()=>{}
+      },
+      concat: {
+        do: ()=>{
           for (let i = 0; i < 2; i++) {
             const start = step.chunks[i].start;
             const end = step.chunks[i].end;
 
             // coloring both concatenated chunks
             for (let k = start; k <= end; k++) {
-              barsRefs[k].style.background = i == 0 ? '#7d7' : '#a0efa0';
+              barsRefs[k].style.background = stepsActionsColor[step.type][i];
             }
           }
-        }
+        },
+        undo: ()=>{}
+      },
+    };
+
+    const step = () => {
+      props.queue.currentStep++;
+
+      // console.log(barsRefs)
+      // TODO: find a way to fix the types problem
+      // TODO: the second item in elements might be value|index, add a condition
+      // TODO: add a final step in the algos implementation
+      // TODO: abstract steps actions and add a revert-step acion to it
+      props.stepsLog.map((step) => {
+        // TODO: manage timing
+        stepsActions[step.type].do();
+        stepsActions[step.type].undo();
       });
     };
 
@@ -107,7 +158,6 @@ export default function AlgoAnimation(props: propsT) {
     props.queue.length = props.stepsLog.length;
     props.queue.currentStep = 0;
   }, []);
-
   return (
     <ul className={`p-1 text-center flex gap-1`}>
       {props.data.map((bar, index) => {
@@ -116,7 +166,8 @@ export default function AlgoAnimation(props: propsT) {
             key={index}
             data-value={bar}
             ref={(el) => barsRefs.push(el)}
-            className={`h-${bar} w-[7px] bg-blue-300`}
+            style={{background: stepsActionsColor.idle[0]}}
+            className={`h-[${bar}px] w-[7px]`}
           ></li>
         );
       })}
