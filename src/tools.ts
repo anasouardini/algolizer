e8c71b6d36e720ff62b887e0fdd0445c11d9c5a7
segfaulty1 <segfaulty@proton.me>
2023-03-01 01:12:24 +0100

    feat: adding radom values methods.

---
+++ src/tools.ts
@@ -0,0 +1,20 @@
+const genid = (length: number) => {
+  let result = '';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+
+const randInt = (mi:number, ma:number) => {
+  let min = Math.ceil(mi)+1;
+  let max = Math.floor(ma);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export { genid, randInt };
