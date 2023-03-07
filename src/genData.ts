5bc86d0245c16413eb3f461dc78042979f1c20d2
segfaulty1 <segfaulty@proton.me>
2023-03-07 22:53:33 +0100

    feat: finished all data generating functions.

--- src/genData.ts
+++ src/genData.ts
@@ -1,16 +1,18 @@
 import { randInt } from './tools';
 
-const step = 2;
-const listLengh = 10 * step;
+const listLengh = 10;
 
-const genList = (cb: (i: number, list: number[]) => number) => {
+const genList = (
+  cb: (i: number, list: number[]) => number,
+  unique: boolean
+) => {
   const list: number[] = [];
   for (let i = 0; i < listLengh; ) {
     const randomInt = cb(i, list);
     // console.log(list)
     // console.log(list.includes(randomInt), randomInt)
-    if (!list.includes(randomInt)) {
-      i += step;
+    if (!list.includes(randomInt) || !unique) {
+      i ++;
       list.push(randomInt);
       // console.log('rand', randomInt)
     }
@@ -20,45 +22,62 @@ const genList = (cb: (i: number, list: number[]) => number) => {
 
 const sorted = () => {
   return genList((i: number, list: number[]) => {
-    const min = list?.[i / step - 1] ?? i; //using last value as the min
+    const min = list?.[i - 1] ?? i; //using last value as the min
     // console.log('list', list);
-    const randomInt = randInt(min, min + 10);
+    const randomInt = randInt(min ? min : 0, min + 10);
     // console.log(min, min + 10, randomInt);
     return randomInt;
-  });
+  }, true);
 };
 const nearlySorted = () => {
   return genList((i: number, list: number[]) => {
     const ordered = Math.random() > 0.2;
     // const ordered = true;
-    const lastInt = list?.[i / step - 1] ?? i;
+    const lastInt = list?.[i - 1] ?? i;
     const min = ordered ? lastInt : i;
-    // const min = list?.[i / step - 1] ?? i;
+    // const min = list?.[i - 1] ?? i;
     // console.log('list', list);
-    const randomInt = randInt(min, min + 10);
+    const randomInt = randInt(min ? min : 0, min + 10);
     // console.log(min, min + 10, randomInt);
     return randomInt;
-  });
+  }, true);
 };
 // TODO: make it litterally is it sounds: first half sorted
 const halfSorted = () => {
   return genList((i: number, list: number[]) => {
     const ordered = Math.random() > 0.5;
-    const min = ordered ? list?.[i / step] ?? i : i;
+    const min = ordered ? list?.[i] ?? i : i;
     // console.log('list', list);
-    const randomInt = randInt(min, 50);
+    const randomInt = randInt(min ? min : 1, 50);
     // console.log(min, min + 10, randomInt);
     return randomInt;
-  });
+  }, true);
 };
 const random = () => {
   return genList((i: number, list: number[]) => {
-    return randInt(0, 50);
-  });
+    return randInt(1, 50);
+  }, true);
 };
-const fewUnique = () => [3, 55, 5, 0, 12, 2, 4, 23, 1];
-const uniformed = () => [3, 55, 5, 0, 12, 2, 4, 23, 1];
-const uniformedWide = () => [3, 55, 5, 0, 12, 2, 4, 23, 1];
+const fewUnique = () => {
+  return genList((i: number, list: number[]) => {
+    // TODO: limit the number of duplication to a min and max
+    const unique = Math.random() > 0.7;
+    const randomInt = unique
+      ? randInt(1, 50)
+      : list[randInt(1, list.length - 1)];
+    return randomInt;
+  }, false);
+};
+const uniformed = () => {
+  const randGap = randInt(2, 5);
+  const sortedList = genList((i: number, list: number[]) => {
+    const newNumber = Math.floor(list?.[i-1] ? list[i - 1] + randGap : 10);
+    return newNumber;
+  }, true);
+
+  // return shuffled list
+  return sortedList.sort(()=>0.5-Math.random());
+};
 
 export default [
   sorted,
@@ -67,5 +86,4 @@ export default [
   random,
   fewUnique,
   uniformed,
-  uniformedWide,
 ];
